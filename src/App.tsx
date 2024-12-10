6130705f4495028daf7a6ae5c2ac97b6918bb0ba
Smart-While <72604311+ramsmart-inno@users.noreply.github.com>
2024-12-10 09:06:46 +0530

    added stamp creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,116 @@
 // Main Index.js or App.js
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, ChangeEvent } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import { Designer } from '@pmee/ui';
 import { text, image, barcodes, dateTime } from '@pmee/schemas';
 import { generatePDF } from './helper';
 import './App.css';
+import { toPng } from 'html-to-image';
 import { base64Image, basePdfUrl, updateBaseImageUrl, updateBasePdfUrl, updateImageDimensions, imageDimensions } from './variables';
+import QRCodeStyling, { DrawType, TypeNumber, Mode, ErrorCorrectionLevel, DotType, CornerSquareType, CornerDotType, FileExtension, Options } from "qr-code-styling";
+
 
 // Main Screen
 const MainScreen = () => {
   const [base64Pdf, setBase64Pdf] = useState<string>('');
+  const [imgSrc, setImgSrc] = useState<string>('');
   const [base64Image, setBase64Image] = useState<string>('');
   const [imageDimensions, setImageDimensions] = useState<{ width: number; height: number }>({
     width: 0,
     height: 0,
   });
+
+  const [options, setOptions] = useState<Options>({
+    width: 300,
+    height: 300,
+    type: 'svg' as DrawType,
+    data: 'http://qr-code-styling.com',
+    image: '/favicon.ico',
+    margin: 10,
+    qrOptions: {
+      typeNumber: 0 as TypeNumber,
+      mode: 'Byte' as Mode,
+      errorCorrectionLevel: 'Q' as ErrorCorrectionLevel
+    },
+    imageOptions: {
+      hideBackgroundDots: true,
+      imageSize: 0.4,
+      margin: 20,
+      crossOrigin: 'anonymous',
+    },
+    dotsOptions: {
+      color: '#222222',
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 0,
+      //   colorStops: [{ offset: 0, color: '#8688B2' }, { offset: 1, color: '#77779C' }]
+      // },
+      type: 'rounded' as DotType
+    },
+    backgroundOptions: {
+      color: '#5FD4F3',
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 0,
+      //   colorStops: [{ offset: 0, color: '#ededff' }, { offset: 1, color: '#e6e7ff' }]
+      // },
+    },
+    cornersSquareOptions: {
+      color: '#222222',
+      type: 'extra-rounded' as CornerSquareType,
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 180,
+      //   colorStops: [{ offset: 0, color: '#25456e' }, { offset: 1, color: '#4267b2' }]
+      // },
+    },
+    cornersDotOptions: {
+      color: '#222222',
+      type: 'dot' as CornerDotType,
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 180,
+      //   colorStops: [{ offset: 0, color: '#00266e' }, { offset: 1, color: '#4060b3' }]
+      // },
+    }
+  });
+  const [fileExt, setFileExt] = useState<FileExtension>("svg");
+  const [qrCode] = useState<QRCodeStyling>(new QRCodeStyling(options));
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      qrCode.append(ref.current);
+    }
+  }, [qrCode, ref]);
+
+  useEffect(() => {
+    if (!qrCode) return;
+    qrCode.update(options);
+  }, [qrCode, options]);
+
+  const onDataChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    if (ref.current) {
+      const dataUrl = await toPng(ref.current); // Convert ref to an image
+      setImgSrc(dataUrl); // Update img src
+    }
+    setOptions(options => ({
+      ...options,
+      data: event.target.value
+    }));
+  };
+
+  const onExtensionChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setFileExt(event.target.value as FileExtension);
+  };
+
+  const onDownloadClick = () => {
+    if (!qrCode) return;
+    qrCode.download({
+      extension: fileExt
+    });
+  };
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>, type: 'pdf' | 'image') => {
     console.log(`File upload triggered for type: ${type}`);
@@ -98,6 +193,31 @@ const MainScreen = () => {
           <p>Height: {imageDimensions.height}px</p>
         </div>
       )}
+
+      <h2>QR Code Generator</h2>
+      <div ref={ref} />
+      <div style={styles.inputWrapper}>
+        <input value={options.data} onChange={onDataChange} style={styles.inputBox} />
+        <select onChange={onExtensionChange} value={fileExt}>
+          <option value="svg">SVG</option>
+          <option value="png">PNG</option>
+          <option value="jpeg">JPEG</option>
+          <option value="webp">WEBP</option>
+        </select>
+        <button onClick={onDownloadClick}>Download</button>
+      </div>
+
+      <h2>Sample Stamp Card</h2>
+      <div style={styles.row}>
+        <img src={imgSrc || "https://placehold.co/100x100"} alt="QR code" style={styles.qrCode} />
+        <div style={styles.details}>
+          <p style={{ ...styles.detailText, ...styles.boldText }}>
+            Signed by: Alice &lt;alice@example.com&gt;
+          </p>
+          <p style={styles.detailText}>2021-06-24 08:00:00 CEST</p>
+          <p style={styles.detailText}>https://example.com</p>
+        </div>
+      </div>
 
       {/* Navigation Links */}
       <div style={{ display: 'flex', gap: '10px', marginTop: '20px' }}>
@@ -252,6 +372,23 @@ const App = () => {
 };
 
 const styles = {
+  inputQR: {
+    margin: "20px 0",
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100",
+  },
+  inputWrapper: {
+    margin: "20px 0",
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    maxWidth: "300px"
+  },
+  inputBox: {
+    flexGrow: 1,
+    marginRight: 20
+  },
   container: {
     display: 'flex',
     flexDirection: 'column',
@@ -272,7 +409,30 @@ const styles = {
     border: 'none',
     borderRadius: '5px',
     cursor: 'pointer',
+  }, 
+  row: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '.2rem', // Similar to 'gap-4'
+    padding: '.2rem', // Similar to 'p-4'
   },
+  qrCode: {
+    width: '4rem',  // Similar to 'w-16'
+    height: '4.4rem', // Similar to 'h-16'
+    marginRight: '.2rem', // Similar to 'mr-4'
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column' as const, 
+  },
+  detailText: {
+    margin: 0, // Reset default margins
+    fontSize: '.6rem', // Similar to 'text-base'
+    lineHeight: '.7rem', // Similar to 'leading-6'
+  },
+  boldText: {
+    fontWeight: 'bold', // Similar to 'font-bold'
+  }
 };
 
 export default App;
